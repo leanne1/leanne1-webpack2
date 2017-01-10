062b2ddcd5ac75522ce19542ab7f2329f372696a
leanne <leanne.tite@gmail.com>
2017-01-10 17:05:18 +0000

    HMR working

--- webpack.config.js
+++ webpack.config.js
@@ -32,9 +32,8 @@ const jsLoaders = [
 // TODO: Make sure to add all vendor libs here
 const vendorLibs = ['react', 'react-dom'];
 
-
 const common = {
-	context: __dirname + '/src',
+	context: resolve(__dirname, 'src'),
 	module: {
 		rules: [
 			{
@@ -68,12 +67,11 @@ if (isDev) {
 			'./app/index.js'
 		],
 		output: {
-			filename: 'bundle.js',
+			filename: '[name].js',
 			path: resolve(__dirname, 'demo'),
 			publicPath: '/',
 		},
-		// devtool: 'cheap-module-eval-source-map',
-		devtool: 'inline-source-map',
+		devtool: 'cheap-module-eval-source-map',
 		performance : {
 			hints : false,
 		},
@@ -119,77 +117,3 @@ if (isProd) {
 		],
 	});
 }
-
-// const { resolve } = require('path');
-// const webpack = require('webpack');
-//
-// module.exports = {
-// 	entry: [
-// 		'react-hot-loader/patch',
-// 		// activate HMR for React
-//
-// 		'webpack-dev-server/client?http://localhost:8080',
-// 		// bundle the client for webpack-dev-server
-// 		// and connect to the provided endpoint
-//
-// 		'webpack/hot/only-dev-server',
-// 		// bundle the client for hot reloading
-// 		// only- means to only hot reload for successful updates
-//
-//
-// 		'./app/index.js'
-// 		// the entry point of our app
-// 	],
-// 	output: {
-// 		filename: 'bundle.js',
-// 		// the output bundle
-//
-// 		path: resolve(__dirname, 'dist'),
-//
-// 		publicPath: '/'
-// 		// necessary for HMR to know where to load the hot update chunks
-// 	},
-//
-// 	context: resolve(__dirname, 'src'),
-//
-// 	devtool: 'inline-source-map',
-//
-// 	devServer: {
-// 		hot: true,
-// 		// enable HMR on the server
-//
-// 		contentBase: resolve(__dirname, 'dist'),
-// 		// match the output path
-//
-// 		publicPath: '/'
-// 		// match the output `publicPath`
-// 	},
-//
-// 	module: {
-// 		rules: [
-// 			{
-// 				test: /\.js$/,
-// 				use: [
-// 					'babel-loader',
-// 				],
-// 				exclude: /node_modules/
-// 			},
-// 			{
-// 				test: /\.css$/,
-// 				use: [
-// 					'style-loader',
-// 					'css-loader?modules',
-// 					'postcss-loader',
-// 				],
-// 			},
-// 		],
-// 	},
-//
-// 	plugins: [
-// 		new webpack.HotModuleReplacementPlugin(),
-// 		// enable HMR globally
-//
-// 		new webpack.NamedModulesPlugin(),
-// 		// prints more readable module names in the browser console on HMR updates
-// 	],
-// };
